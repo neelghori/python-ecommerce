26db3b25ec55169e29932cc89f791afd0735bd1b
neelghori <71180766+neelghori@users.noreply.github.com>
2024-08-26 21:58:20 +0530

    wip

--- vercel_build.sh
+++ vercel_build.sh
@@ -2,5 +2,5 @@
 # pip install -r requirements.txt
 # python manage.py collectstatic --no-input
 
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip3 install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
